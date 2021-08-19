3878c6974f56381ad619cca431871a5be9304734
Writer Of Worlds <57270802+WriterOfWorlds@users.noreply.github.com>
2021-08-19 12:36:52 -0700

    Added SB support for most axes

--- src/main/java/net/mcreator/fireemblem/procedures/ShieldBreakerSkillProcedure.java
+++ src/main/java/net/mcreator/fireemblem/procedures/ShieldBreakerSkillProcedure.java
@@ -1,14 +1,53 @@
 package net.mcreator.fireemblem.procedures;
 
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+
+import net.minecraft.world.World;
+import net.minecraft.item.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.Entity;
 
+import net.mcreator.fireemblem.item.ForgedNetherAxeItem;
+import net.mcreator.fireemblem.item.ForgedIronAxeItem;
+import net.mcreator.fireemblem.item.ForgedGoldAxeItem;
+import net.mcreator.fireemblem.item.ForgedDiamondAxeItem;
 import net.mcreator.fireemblem.FireEmblemModVariables;
 import net.mcreator.fireemblem.FireEmblemMod;
 
 import java.util.Map;
+import java.util.HashMap;
 
 public class ShieldBreakerSkillProcedure {
+	@Mod.EventBusSubscriber
+	private static class GlobalTrigger {
+		@SubscribeEvent
+		public static void onEntityAttacked(LivingHurtEvent event) {
+			if (event != null && event.getEntity() != null) {
+				Entity entity = event.getEntity();
+				Entity sourceentity = event.getSource().getTrueSource();
+				double i = entity.getPosX();
+				double j = entity.getPosY();
+				double k = entity.getPosZ();
+				double amount = event.getAmount();
+				World world = entity.world;
+				Map<String, Object> dependencies = new HashMap<>();
+				dependencies.put("x", i);
+				dependencies.put("y", j);
+				dependencies.put("z", k);
+				dependencies.put("amount", amount);
+				dependencies.put("world", world);
+				dependencies.put("entity", entity);
+				dependencies.put("sourceentity", sourceentity);
+				dependencies.put("event", event);
+				executeProcedure(dependencies);
+			}
+		}
+	}
 	public static void executeProcedure(Map<String, Object> dependencies) {
 		if (dependencies.get("entity") == null) {
 			if (!dependencies.containsKey("entity"))
@@ -22,9 +61,32 @@ public class ShieldBreakerSkillProcedure {
 		}
 		Entity entity = (Entity) dependencies.get("entity");
 		Entity sourceentity = (Entity) dependencies.get("sourceentity");
-		if (((sourceentity.getCapability(FireEmblemModVariables.PLAYER_VARIABLES_CAPABILITY, null)
-				.orElse(new FireEmblemModVariables.PlayerVariables())).hasSB)) {
-			((PlayerEntity) entity).disableShield((true));
+		if ((entity instanceof ServerPlayerEntity)) {
+			if ((sourceentity instanceof ServerPlayerEntity)) {
+				if (((((((((sourceentity instanceof LivingEntity) ? ((LivingEntity) sourceentity).getHeldItemMainhand() : ItemStack.EMPTY)
+						.getItem() == Items.STONE_AXE)
+						|| (((sourceentity instanceof LivingEntity) ? ((LivingEntity) sourceentity).getHeldItemMainhand() : ItemStack.EMPTY)
+								.getItem() == Items.IRON_AXE))
+						|| (((sourceentity instanceof LivingEntity) ? ((LivingEntity) sourceentity).getHeldItemMainhand() : ItemStack.EMPTY)
+								.getItem() == Items.GOLDEN_AXE))
+						|| (((sourceentity instanceof LivingEntity) ? ((LivingEntity) sourceentity).getHeldItemMainhand() : ItemStack.EMPTY)
+								.getItem() == Items.DIAMOND_AXE))
+						|| (((sourceentity instanceof LivingEntity) ? ((LivingEntity) sourceentity).getHeldItemMainhand() : ItemStack.EMPTY)
+								.getItem() == Items.NETHERITE_AXE))
+						|| ((((((sourceentity instanceof LivingEntity) ? ((LivingEntity) sourceentity).getHeldItemMainhand() : ItemStack.EMPTY)
+								.getItem() == ForgedIronAxeItem.block)
+								|| (((sourceentity instanceof LivingEntity) ? ((LivingEntity) sourceentity).getHeldItemMainhand() : ItemStack.EMPTY)
+										.getItem() == ForgedGoldAxeItem.block))
+								|| (((sourceentity instanceof LivingEntity) ? ((LivingEntity) sourceentity).getHeldItemMainhand() : ItemStack.EMPTY)
+										.getItem() == ForgedDiamondAxeItem.block))
+								|| (((sourceentity instanceof LivingEntity) ? ((LivingEntity) sourceentity).getHeldItemMainhand() : ItemStack.EMPTY)
+										.getItem() == ForgedNetherAxeItem.block)))) {
+					if (((sourceentity.getCapability(FireEmblemModVariables.PLAYER_VARIABLES_CAPABILITY, null)
+							.orElse(new FireEmblemModVariables.PlayerVariables())).hasSB)) {
+						((PlayerEntity) entity).disableShield((true));
+					}
+				}
+			}
 		}
 	}
 }
