74e718571e79509dfb56d27a5f5e80519f7c3f67
Writer Of Worlds <57270802+WriterOfWorlds@users.noreply.github.com>
2021-09-09 20:40:48 -0700

    Made game actually playable (Somewhat) in survival

---
+++ src/main/java/net/mcreator/fireemblem/item/LevelGemItem.java
@@ -0,0 +1,47 @@
+
+package net.mcreator.fireemblem.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.fireemblem.FireEmblemModElements;
+
+@FireEmblemModElements.ModElement.Tag
+public class LevelGemItem extends FireEmblemModElements.ModElement {
+	@ObjectHolder("fire_emblem:level_gem")
+	public static final Item block = null;
+	public LevelGemItem(FireEmblemModElements instance) {
+		super(instance, 195);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MATERIALS).maxStackSize(64).rarity(Rarity.UNCOMMON));
+			setRegistryName("level_gem");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
