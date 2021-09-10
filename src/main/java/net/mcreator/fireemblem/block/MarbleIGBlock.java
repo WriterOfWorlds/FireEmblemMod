ca05a0fc49c4c0ddc6d38cdcc2234495ba7f7bce
Writer Of Worlds <57270802+WriterOfWorlds@users.noreply.github.com>
2021-09-09 19:51:44 -0700

    Fixed Achievements and added Marble

---
+++ src/main/java/net/mcreator/fireemblem/block/MarbleIGBlock.java
@@ -0,0 +1,71 @@
+
+package net.mcreator.fireemblem.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.common.ToolType;
+
+import net.minecraft.world.IBlockReader;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.pathfinding.PathNodeType;
+import net.minecraft.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.entity.MobEntity;
+import net.minecraft.block.material.MaterialColor;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.fireemblem.FireEmblemModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@FireEmblemModElements.ModElement.Tag
+public class MarbleIGBlock extends FireEmblemModElements.ModElement {
+	@ObjectHolder("fire_emblem:marble_ig")
+	public static final Block block = null;
+	public MarbleIGBlock(FireEmblemModElements instance) {
+		super(instance, 192);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(ItemGroup.BUILDING_BLOCKS)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.ROCK).sound(SoundType.STONE).hardnessAndResistance(1f, 10f).setLightLevel(s -> 0).harvestLevel(2)
+					.harvestTool(ToolType.PICKAXE).setRequiresTool());
+			setRegistryName("marble_ig");
+		}
+
+		@Override
+		public int getOpacity(BlockState state, IBlockReader worldIn, BlockPos pos) {
+			return 15;
+		}
+
+		@Override
+		public MaterialColor getMaterialColor() {
+			return MaterialColor.WHITE_TERRACOTTA;
+		}
+
+		@Override
+		public PathNodeType getAiPathNodeType(BlockState state, IBlockReader world, BlockPos pos, MobEntity entity) {
+			return PathNodeType.DAMAGE_OTHER;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 1));
+		}
+	}
+}
--- src/main/java/net/mcreator/fireemblem/procedures/GiveForgeProcedure.java
+++ src/main/java/net/mcreator/fireemblem/procedures/GiveForgeProcedure.java
@@ -2,7 +2,7 @@ package net.mcreator.fireemblem.procedures;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.event.TickEvent;
 
 import net.minecraft.world.server.ServerWorld;
 import net.minecraft.world.World;
@@ -10,6 +10,7 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.advancements.AdvancementProgress;
 import net.minecraft.advancements.Advancement;
@@ -33,22 +34,22 @@ public class GiveForgeProcedure {
 	@Mod.EventBusSubscriber
 	private static class GlobalTrigger {
 		@SubscribeEvent
-		public static void onItemCrafted(PlayerEvent.ItemCraftedEvent event) {
-			Entity entity = event.getPlayer();
-			World world = entity.world;
-			double i = entity.getPosX();
-			double j = entity.getPosY();
-			double k = entity.getPosZ();
-			ItemStack itemStack = event.getCrafting();
-			Map<String, Object> dependencies = new HashMap<>();
-			dependencies.put("x", i);
-			dependencies.put("y", j);
-			dependencies.put("z", k);
-			dependencies.put("world", world);
-			dependencies.put("entity", entity);
-			dependencies.put("itemstack", itemStack);
-			dependencies.put("event", event);
-			executeProcedure(dependencies);
+		public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
+			if (event.phase == TickEvent.Phase.END) {
+				Entity entity = event.player;
+				World world = entity.world;
+				double i = entity.getPosX();
+				double j = entity.getPosY();
+				double k = entity.getPosZ();
+				Map<String, Object> dependencies = new HashMap<>();
+				dependencies.put("x", i);
+				dependencies.put("y", j);
+				dependencies.put("z", k);
+				dependencies.put("world", world);
+				dependencies.put("entity", entity);
+				dependencies.put("event", event);
+				executeProcedure(dependencies);
+			}
 		}
 	}
 	public static void executeProcedure(Map<String, Object> dependencies) {
@@ -57,18 +58,35 @@ public class GiveForgeProcedure {
 				FireEmblemMod.LOGGER.warn("Failed to load dependency entity for procedure GiveForge!");
 			return;
 		}
-		if (dependencies.get("itemstack") == null) {
-			if (!dependencies.containsKey("itemstack"))
-				FireEmblemMod.LOGGER.warn("Failed to load dependency itemstack for procedure GiveForge!");
-			return;
-		}
 		Entity entity = (Entity) dependencies.get("entity");
-		ItemStack itemstack = (ItemStack) dependencies.get("itemstack");
-		if ((((itemstack).getItem() == ForgedIronSwordItem.block) || (((itemstack).getItem() == ForgedIronAxeItem.block)
-				|| (((itemstack).getItem() == ForgedGoldSwordItem.block) || (((itemstack).getItem() == ForgedGoldAxeItem.block)
-						|| (((itemstack).getItem() == ForgedDiamondSwordItem.block) || (((itemstack).getItem() == ForgedDiamondAxeItem.block)
-								|| (((itemstack).getItem() == ForgedNetherSwordItem.block) || (((itemstack).getItem() == ForgedNetherAxeItem.block)
-										|| ((itemstack).getItem() == ForgedSilverSwordItem.block)))))))))) {
+		if ((((entity instanceof PlayerEntity) ? ((PlayerEntity) entity).inventory.hasItemStack(new ItemStack(ForgedIronSwordItem.block)) : false)
+				|| (((entity instanceof PlayerEntity)
+						? ((PlayerEntity) entity).inventory.hasItemStack(new ItemStack(ForgedIronAxeItem.block))
+						: false)
+						|| (((entity instanceof PlayerEntity)
+								? ((PlayerEntity) entity).inventory.hasItemStack(new ItemStack(ForgedGoldSwordItem.block))
+								: false)
+								|| (((entity instanceof PlayerEntity)
+										? ((PlayerEntity) entity).inventory.hasItemStack(new ItemStack(ForgedGoldAxeItem.block))
+										: false)
+										|| (((entity instanceof PlayerEntity)
+												? ((PlayerEntity) entity).inventory.hasItemStack(new ItemStack(ForgedDiamondSwordItem.block))
+												: false)
+												|| (((entity instanceof PlayerEntity)
+														? ((PlayerEntity) entity).inventory.hasItemStack(new ItemStack(ForgedDiamondAxeItem.block))
+														: false)
+														|| (((entity instanceof PlayerEntity)
+																? ((PlayerEntity) entity).inventory
+																		.hasItemStack(new ItemStack(ForgedNetherSwordItem.block))
+																: false)
+																|| (((entity instanceof PlayerEntity)
+																		? ((PlayerEntity) entity).inventory
+																				.hasItemStack(new ItemStack(ForgedNetherAxeItem.block))
+																		: false)
+																		|| ((entity instanceof PlayerEntity)
+																				? ((PlayerEntity) entity).inventory
+																						.hasItemStack(new ItemStack(ForgedSilverSwordItem.block))
+																				: false)))))))))) {
 			if ((!(((entity instanceof ServerPlayerEntity) && (entity.world instanceof ServerWorld))
 					? ((ServerPlayerEntity) entity).getAdvancements()
 							.getProgress(((MinecraftServer) ((ServerPlayerEntity) entity).server).getAdvancementManager()
